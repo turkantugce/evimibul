3293aefbaf750f28e1ad4f87489f658a7fd682a4
turkantugce <131966002+turkantugce@users.noreply.github.com>
2025-10-08 11:00:24 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1AMyCZ1YQ54cZgwY9vF2LHfgGUD3yibg",
-  authDomain: "petadoptionapp-ec77c.firebaseapp.com",
-  projectId: "petadoptionapp-ec77c",
-  storageBucket: "petadoptionapp-ec77c.firebasestorage.app",
-  messagingSenderId: "880779689169",
-  appId: "1:880779689169:web:1fa5d03cf1e178cecdd9f0",
-  measurementId: "G-P0DJV9R9MJ"
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-"
 };
 
 // Initialize Firebase
